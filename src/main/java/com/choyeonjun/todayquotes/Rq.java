34a7468e956837d01ffa6361ca8f7b7fa0982b3b
YeonJun Cho <micayel1219@gmail.com>
2022-07-12 11:36:24 +0900

    rq에 Map 도입

--- src/main/java/com/choyeonjun/todayquotes/Rq.java
+++ src/main/java/com/choyeonjun/todayquotes/Rq.java
@@ -1,37 +1,47 @@
 package com.choyeonjun.todayquotes;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     String url;
     String path;
-    String queryStr;
+    Map<String, String> queryParams;
 
     public Rq(String url) {
         this.url = url;
         String[] urlBits = url.split("\\?", 2);
         this.path = urlBits[0];
+        this.queryParams = new HashMap<>();
 
+        //오입력시 인덱스 에러 방지
         if (urlBits.length == 2) {
-            this.queryStr = urlBits[1];
+            String queryStr = urlBits[1];
+
+            String[] paramBits = queryStr.split("&");
+
+            for(String paramBit : paramBits){
+                String[] paramNameAndValue = paramBit.split("=", 2);
+
+                if(paramNameAndValue.length == 1) continue;
+
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
+            }
         }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-        if (queryStr == null) {
+        if (!queryParams.containsKey(paramName)) {
             return defaultValue;
         }
 
-        String[] bits = queryStr.split("&");
+        String paramValue = queryParams.get(paramName);
+        if(paramName.length() == 0) return defaultValue;
 
-        for (String urlBit : bits) {
-            String[] paramNameAndValue = urlBit.split("=", 2);
-            String paramName_ = paramNameAndValue[0];
-            String paramValue = paramNameAndValue[1];
-
-            if (paramName.equals(paramName_)) {
-                return Integer.parseInt(paramValue);
-            }
-        }
-
-        return defaultValue;
+        return Integer.parseInt(paramValue);
     }
 
     public String getPath() {
